bdaf4ae80b34bf96ce1cf7e81ac81a4c2a1c9b7c
Suzaina Khanum <suzainakhanumbca24@rvu.edu.in>
2025-08-12 10:25:06 +0530

    Create program1.java

---
+++ lab1/program1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(system.in);
+        System.out.print("enter the number");
+        int num =sc.nextInt();
+        if(num %2==0);{
+            System.out.println("even");
+        else{
+            System.ouy.println("odd");
+        
+        }
+    }
+}
