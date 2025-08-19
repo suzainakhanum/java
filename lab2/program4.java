067397e6d133eecc2cdae666dd7ba4af828eaf32
Suzaina Khanum <suzainakhanumbca24@rvu.edu.in>
2025-08-19 10:57:11 +0530

    program4.java

---
+++ lab2/program4.java
@@ -0,0 +1,17 @@
+package Animal;
+
+class Animal {
+	void sound() {
+		System.out.println("Animal makes sound");
+	}
+}
+class Dog extends Animal{
+	void sound() {
+		System.out.println("Dog barks");
+	}
+	public static void main(String[]args) {
+		Dog d = new Dog();
+		d.sound();
+	}
+	
+}
